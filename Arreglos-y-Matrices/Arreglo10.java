5f6becbe0dca2dad2bf686b552a790361f3aea34
Cristianxtbt <cristian3145962276@gmail.com>
2026-09-15 13:39:45 +0000

    ArreglosYMatrices

--- Arreglos-y-Matrices/Arreglo10.java
+++ Arreglos-y-Matrices/Arreglo10.java
@@ -11,11 +11,22 @@ public class Arreglo10 {
 
         //ARREGLO B
         int bLength = a.length / 2;
+
+        //SI ES IMPAR, SUMAMOS 1 PARA QUE ENTRE EL ELEMENTO DEL MEDIO
+        if (a.length % 2 != 0) {
+            bLength = bLength + 1;
+        }
+
         int[] b = new int[bLength];
 
-        //SUMAMOS
-        for (int i = 0; i < b.length; i++) {
+        //SUMAMOS LAS PAREJAS DE OPUESTOS
+        for (int i = 0; i < a.length / 2; i++) {
             b[i] = a[i] + a[a.length - 1 - i];
+        }
+
+        //SI ES IMPAR, EL ELEMENTO DEL MEDIO SE COPIA SOLO (NO TIENE PAREJA)
+        if (a.length % 2 != 0) {
+            b[b.length - 1] = a[a.length / 2];
         }
 
         //MOSTRAR AREGLO B
